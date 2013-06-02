eceaba4a0fbc54c82f80c7ff73d5c4d7d5ca8c4b
Sam Rawlins <sam.rawlins@gmail.com>
2013-06-02 14:59:22 -0700

    Ruby 2.1 guard

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require_relative "../lib/objectspace/stats"
 require "yaml"
 
+if RbConfig::CONFIG["MAJOR"].to_i < 2 || RbConfig::CONFIG["MINOR"].to_i < 1
+  warn "Error: ObjectStats requires Ruby 2.1 or greater"
+  exit 1
+end
+
 def allocate_a_string_from_spec_helper
   return "a string from spec_helper"
 end
